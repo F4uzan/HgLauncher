9882687643b83c37229e34c22ca37e80965687c6
F4uzan <derpish123@gmail.com>
2016-03-16 10:16:31 +0700

    Initial commit
    Pushed changes from local remote to origin

---
+++ app/src/main/java/f4/hubby/AppDetail.java
@@ -0,0 +1,9 @@
+package f4.hubby;
+
+import android.graphics.drawable.Drawable;
+
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
---
+++ app/src/main/java/f4/hubby/MainActivity.java
@@ -0,0 +1,153 @@
+package f4.hubby;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PackageManager manager;
+    private List<AppDetail> apps;
+    private NestedListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        loadApps();
+        loadListView();
+        addClickListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadApps(){
+        manager = getPackageManager();
+        apps = new ArrayList<>();
+
+        Intent i = new Intent(Intent.ACTION_MAIN, null);
+        i.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> availableActivities = manager.queryIntentActivities(i, 0);
+        for(ResolveInfo ri:availableActivities){
+            AppDetail app = new AppDetail();
+            app.label = ri.loadLabel(manager);
+            app.name = ri.activityInfo.packageName;
+            app.icon = ri.activityInfo.loadIcon(manager);
+            apps.add(app);
+        }
+    }
+
+    private void loadListView(){
+        list = (NestedListView) findViewById(R.id.apps_list);
+
+        ArrayAdapter<AppDetail> adapter = new ArrayAdapter<AppDetail>(this,
+                R.layout.app_list,
+                apps) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null){
+                    convertView = getLayoutInflater().inflate(R.layout.app_list, null);
+                }
+
+                ImageView appIcon = (ImageView)convertView.findViewById(R.id.item_app_icon);
+                appIcon.setImageDrawable(apps.get(position).icon);
+
+                TextView appLabel = (TextView)convertView.findViewById(R.id.item_app_label);
+                appLabel.setText(apps.get(position).label);
+
+                TextView appName = (TextView)convertView.findViewById(R.id.item_app_name);
+                appName.setText(apps.get(position).name);
+
+                return convertView;
+            }
+        };
+
+        list.setAdapter(adapter);
+        setListViewHeightBasedOnChildren(list);
+    }
+
+    private void addClickListener(){
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos,
+                                    long id) {
+                Intent i = manager.getLaunchIntentForPackage(apps.get(pos).name.toString());
+                MainActivity.this.startActivity(i);
+            }
+        });
+    }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(), View.MeasureSpec.AT_MOST);
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+        //I added this to try to fix half hidden row
+        totalHeight++;
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
+
+}
