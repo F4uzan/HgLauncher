0fe6af7253d776d66003d436df6b5f03989a69c3
F4uzan <derpish123@gmail.com>
2016-03-19 14:09:29 +0700

    Fix force close in 5.0+ caused by MenuPreferenceCompat
    We're extending ListPreference provided by MaterialPreference before, make another class that extends native ListPreference so everything doesn't crash.

---
+++ app/src/main/java/f4/hubby/MenuPreference.java
@@ -0,0 +1,54 @@
+package f4.hubby;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.support.v7.widget.PopupMenu;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MenuPreference extends ListPreference {
+    private View anchor;
+
+    public MenuPreference(Context context) {
+        super(context);
+    }
+
+    public MenuPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        return anchor = super.onCreateView(parent);
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        final PopupMenu popup = new PopupMenu(getContext(), anchor, Gravity.TOP);
+        final Menu menu = popup.getMenu();
+
+        for (int i = 0; i < getEntries().length; i++) {
+            MenuItem item = menu.add(1, i, Menu.NONE, getEntries()[i]);
+            item.setChecked(item.getTitle().equals(getEntry())); // 1
+        }
+        menu.setGroupCheckable(1, true, true); // 2
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                popup.dismiss();
+                String value = getEntryValues()[item.getItemId()].toString();
+                if (callChangeListener(value))
+                    setValue(value);
+                return true;
+            }
+        });
+        popup.show();
+    }
+
+}
