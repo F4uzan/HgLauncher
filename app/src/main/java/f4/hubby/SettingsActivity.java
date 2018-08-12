1e251c6b6ee8d1d749597b8b1cd4abb42b8148bb
F4uzan <f4@this.is.f4>
2018-08-12 18:16:27 +0100

    Add hidden app management screen
    Super simple and 'just works', as usual. There isn't much visual cues
    inside the screen/fragment itself, and pretty much all of the code is
    scrapped straight from MainActivity — might want to refactor this later.
    
    *TODO: Maybe have an overflow button for each entry instead of having a
    long press like the launcher's app list?

--- app/src/main/java/f4/hubby/SettingsActivity.java
+++ app/src/main/java/f4/hubby/SettingsActivity.java
@@ -1,20 +1,42 @@
 package f4.hubby;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.util.ArraySet;
 import android.support.v7.app.ActionBar;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
+import f4.hubby.helpers.RecyclerClick;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
 
@@ -125,7 +147,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     protected boolean isValidFragment(String fragmentName) {
         return PreferenceFragment.class.getName().equals(fragmentName)
                 || CustomizePreferenceFragment.class.getName().equals(fragmentName)
-                || AboutPreferenceFragment.class.getName().equals(fragmentName);
+                || AboutPreferenceFragment.class.getName().equals(fragmentName)
+                || HiddenAppFragment.class.getName().equals(fragmentName);
     }
 
     /**
@@ -177,6 +200,108 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                 return true;
             }
             return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class HiddenAppFragment extends PreferenceFragment {
+        ArrayList<AppDetail> appList = new ArrayList<>();
+        Set<String> excludedAppList = new ArraySet<>();
+        AppAdapter apps = new AppAdapter(appList);
+        RecyclerView list;
+
+        public HiddenAppFragment() {
+        }
+
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_hidden_apps, container, false);
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            list = getActivity().findViewById(R.id.ex_apps_list);
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+            LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity(),
+                    LinearLayoutManager.VERTICAL, false);
+
+            list.setAdapter(apps);
+            list.setLayoutManager(mLayoutManager);
+            list.setItemAnimator(new DefaultItemAnimator());
+
+            // Get our app list.
+            excludedAppList.addAll(prefs.getStringSet("hidden_apps", excludedAppList));
+
+            // Start initialisation.
+            loadHiddenApps();
+            addListeners();
+        }
+
+        private void loadHiddenApps() {
+            PackageManager manager = getActivity().getPackageManager();
+            appList.clear();
+            apps.notifyDataSetChanged();
+
+            for (String packageName : excludedAppList) {
+                ApplicationInfo appInfo;
+                Drawable icon = null;
+                try {
+                    appInfo = manager.getApplicationInfo(packageName, 0);
+                    icon = manager.getApplicationIcon(packageName);
+                } catch (PackageManager.NameNotFoundException e) {
+                    appInfo = null;
+                }
+                String appName = manager.getApplicationLabel(appInfo).toString();
+                AppDetail app = new AppDetail(icon, appName, packageName);
+                appList.add(app);
+                apps.notifyItemInserted(appList.size() - 1);
+            }
+
+            // Update our view cache size, now that we have got all apps on the list.
+            list.setItemViewCacheSize(appList.size() - 1);
+        }
+
+        private void addListeners() {
+            final SharedPreferences.Editor editPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity()).edit();
+
+            RecyclerClick.addTo(list).setOnItemLongClickListener(new RecyclerClick.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClicked(RecyclerView recyclerView, int position, View v) {
+                    // Parse package URI for use in uninstallation and package info call.
+                    final String packageName = appList.get(position).getPackageName();
+                    final Uri packageNameUri = Uri.parse("package:" + packageName);
+
+                    // Inflate the app menu.
+                    PopupMenu appMenu = new PopupMenu(getActivity(), v);
+                    appMenu.getMenuInflater().inflate(R.menu.menu_hidden_app, appMenu.getMenu());
+                    appMenu.show();
+
+                    // Set listener for the menu.
+                    appMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.action_info:
+                                    startActivity(new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                            packageNameUri));
+                                    break;
+                                case R.id.action_uninstall:
+                                    startActivity(new Intent(Intent.ACTION_DELETE, packageNameUri));
+                                    break;
+                                case R.id.action_show:
+                                    // Add the app's package name to the exclusion list.
+                                    excludedAppList.remove(packageName);
+                                    editPrefs.putStringSet("hidden_apps", excludedAppList).apply();
+                                    // Reload the app list!
+                                    loadHiddenApps();
+                                    break;
+                            }
+                            return true;
+                        }
+                    });
+                    return false;
+                }
+            });
         }
     }
 }
