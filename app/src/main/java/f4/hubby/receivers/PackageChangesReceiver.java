7169d3ed47778e43e9f97492a9a4b2963feb888b
F4uzan <f4@this.is.f4>
2018-08-22 09:08:26 +0100

    Don't listed to PACKAGE_FULLY_REMOVED
    I found that it's rather unnecessary to do this.

--- app/src/main/java/f4/hubby/receivers/PackageChangesReceiver.java
+++ app/src/main/java/f4/hubby/receivers/PackageChangesReceiver.java
@@ -7,18 +7,15 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 public class PackageChangesReceiver extends BroadcastReceiver {
-    Context context;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
-        this.context = context;
 
         // Receive intent from broadcast and simply let the launcher know it needs a refresh.
         if (intent.getAction().equals("android.intent.action.PACKAGE_REMOVED")
                 || intent.getAction().equals("android.intent.action.PACKAGE_ADDED")
-                || intent.getAction().equals("android.intent.action.PACKAGE_CHANGED")
-                || intent.getAction().equals("android.intent.action.PACKAGE_FULLY_REMOVED")) {
+                || intent.getAction().equals("android.intent.action.PACKAGE_CHANGED")) {
             editor.putBoolean("refreshAppList", true).apply();
         }
     }
