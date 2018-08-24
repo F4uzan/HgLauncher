33d6cd241bfa087d13dd949d0a32165b0ccbf141
F4uzan <f4@this.is.f4>
2018-08-24 11:10:28 +0100

    Don't call loadApps() on app hide
    loadApps() is expensive. Repeatedly calling for it causes noticeable
    slow downs — and this is further exacerbated when the user uses
    underpowered hardware.

--- app/src/main/java/f4/hubby/MainActivity.java
+++ app/src/main/java/f4/hubby/MainActivity.java
@@ -272,7 +272,12 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
             case "hidden_apps":
                 excludedAppList.clear();
                 excludedAppList.addAll(prefs.getStringSet("hidden_apps", excludedAppList));
-                loadApps(true);
+                for (String packageName : excludedAppList) {
+                    AppDetail removedPackage = new AppDetail(null, null, packageName);
+                    if (appList.contains(removedPackage)) {
+                        appList.remove(removedPackage);
+                    }
+                }
                 apps.setUpdateFilter(true);
                 break;
             case "icon_hide_switch":
