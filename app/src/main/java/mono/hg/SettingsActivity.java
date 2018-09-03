557170a28ba08bfae483ab9e5c880831f92c7727
F4uzan <f4@this.is.f4>
2018-09-03 08:47:18 +0100

    Move CustomizePreferenceFragment out of SettingsActivity
    Make things more manageable here.

--- app/src/main/java/mono/hg/SettingsActivity.java
+++ app/src/main/java/mono/hg/SettingsActivity.java
@@ -1,24 +1,14 @@
 package mono.hg;
 
-import android.Manifest;
-import android.annotation.TargetApi;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActivityCompat;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import mono.hg.fragments.CustomizePreferenceFragment;
 
 public class SettingsActivity extends com.fnp.materialpreferences.PreferenceActivity {
 
@@ -79,166 +69,5 @@ public class SettingsActivity extends com.fnp.materialpreferences.PreferenceActi
     protected boolean isValidFragment(String fragmentName) {
         return PreferenceFragment.class.getName().equals(fragmentName)
                 || CustomizePreferenceFragment.class.getName().equals(fragmentName);
-    }
-
-    /**
-     * It is used when the activity is showing a two-pane settings UI.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class CustomizePreferenceFragment extends
-            com.fnp.materialpreferences.PreferenceFragment {
-
-        @Override
-        public int addPreferencesFromResource() {
-            return R.xml.pref_customization;
-        }
-
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setHasOptionsMenu(true);
-
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-
-            final Preference versionMenu = findPreference("version_key");
-
-            if (prefs.getBoolean("is_grandma", false)) {
-                versionMenu.setEnabled(false);
-                versionMenu.setTitle(R.string.version_key_name);
-            }
-
-            versionMenu.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                int counter = 9;
-                SharedPreferences.Editor editor =  PreferenceManager.getDefaultSharedPreferences(getActivity()).edit();
-                Toast counterToast;
-
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    if (counter > 0) {
-                        counter--;
-                    } else if (counter == 0) {
-                        editor.putBoolean("is_grandma", true).apply();
-                        versionMenu.setEnabled(false);
-                        versionMenu.setTitle(R.string.version_key_name);
-                    }
-
-                    if (counter < 7 && counter > 1) {
-                        if (counterToast != null) {
-                            counterToast.cancel();
-                        }
-                        counterToast = Toast.makeText(getActivity(),
-                                String.format(getString(R.string.version_key_toast_plural), counter), Toast.LENGTH_SHORT);
-                        counterToast.show();
-                    } else if (counter == 1) {
-                        if (counterToast != null) {
-                            counterToast.cancel();
-                        }
-                        counterToast = Toast.makeText(getActivity(), R.string.version_key_toast, Toast.LENGTH_SHORT);
-                        counterToast.show();
-                    }
-                    return false;
-                }
-            });
-
-            Preference hiddenAppsMenu = findPreference("hidden_apps_menu");
-            hiddenAppsMenu.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    Intent intent = new Intent(getActivity(), HiddenAppsActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-                    return false;
-                }
-            });
-
-            Preference backupMenu = findPreference("backup");
-            backupMenu.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    if (hasStoragePermission()) {
-                        Intent intent = new Intent(getActivity(), BackupRestoreActivity.class);
-                        intent.putExtra("isRestore", false);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
-                        return false;
-                    } else {
-                        return false;
-                    }
-                }
-            });
-
-            Preference restoreMenu = findPreference("restore");
-            restoreMenu.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    if (hasStoragePermission()) {
-                        Intent intent = new Intent(getActivity(), BackupRestoreActivity.class);
-                        intent.putExtra("isRestore", true);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
-                        return false;
-                    } else {
-                        return false;
-                    }
-                }
-            });
-
-            final ListPreference iconList = (ListPreference) findPreference("icon_pack");
-            setIconList(iconList);
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
-
-        protected void setIconList(ListPreference list) {
-            PackageManager manager = getActivity().getPackageManager();
-            List<String> entries = new ArrayList<>();
-            List<String> entryValues = new ArrayList<>();
-
-            // Get default value.
-            entries.add(getString(R.string.icon_pack_default));
-            entryValues.add(getString(R.string.icon_pack_default_value));
-
-            // Fetch all available icon pack.
-            Intent intent = new Intent("org.adw.launcher.THEMES");
-            List<ResolveInfo> infos = manager.queryIntentActivities (intent,
-                    PackageManager.GET_META_DATA);
-            for (ResolveInfo info : infos) {
-                ActivityInfo activityInfo = info.activityInfo;
-                String packageName = activityInfo.packageName;
-                String appName = activityInfo.loadLabel(manager).toString();
-                entries.add(appName);
-                entryValues.add(packageName);
-            }
-
-            CharSequence[] finalEntries = entries.toArray(new CharSequence[entries.size()]);
-            CharSequence[] finalEntryValues = entryValues.toArray(new CharSequence[entryValues.size()]);
-
-            list.setEntries(finalEntries);
-            list.setEntryValues(finalEntryValues);
-        }
-
-        // Used to check for storage permission.
-        // Throws true when API is less than M.
-        private boolean hasStoragePermission() {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (getContext().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(getActivity(),
-                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 4200);
-                } else {
-                    return true;
-                }
-            } else {
-                return true;
-            }
-            return false;
-        }
     }
 }
