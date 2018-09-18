b0d88161e2099b121f0b5fff2eee64e944e48070
F4uzan <f4@this.is.f4>
2018-09-18 12:07:59 +0100

    Stop highlighting XMLs
    I find it pointless now that I put my mind to it. No one's going to
    mistake that file icon anyhow.

--- app/src/main/java/mono/hg/adapters/FileFolderAdapter.java
+++ app/src/main/java/mono/hg/adapters/FileFolderAdapter.java
@@ -1,7 +1,6 @@
 package mono.hg.adapters;
 
 import android.content.Context;
-import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -50,10 +49,6 @@ public class FileFolderAdapter extends BaseAdapter {
         }
 
         holder.name.setText(files.get(position).getName());
-
-        if (files.get(position).shouldHighlight()) {
-            holder.name.setTypeface(null, Typeface.BOLD);
-        }
 
         return convertView;
     }
