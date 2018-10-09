7cb4e490911138d5b4b1d6cba4e5e6af81e77427
F4uzan <f4@this.is.f4>
2018-10-09 12:28:06 +0100

    Minor changes to PackageChangesReceiver
    * Better handle PACKAGE_CHANGED events by coupling it to isReplacing
    check.
    * Use PreferenceHelper here. The assumption here is that
    PreferenceHelper will still be available as long as the launcher didn't
    die — and if the launcher dies then it'll just recreate itself,
    rendering the receiver useless.

--- app/src/main/java/mono/hg/receivers/PackageChangesReceiver.java
+++ app/src/main/java/mono/hg/receivers/PackageChangesReceiver.java
@@ -3,24 +3,26 @@ package mono.hg.receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+
+import mono.hg.helpers.PreferenceHelper;
 
 public class PackageChangesReceiver extends BroadcastReceiver {
 
     @Override public void onReceive(Context context, Intent intent) {
-        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
-
         if (intent.getAction() != null && intent.getData() != null) {
             String packageName = intent.getData().getEncodedSchemeSpecificPart();
-            boolean isReplacing = intent.getBooleanExtra(Intent.EXTRA_REPLACING, false);
+            boolean isReplacing = false;
+
+            if (intent.getAction().equals("android.intent.action.PACKAGE_CHANGED")
+                    && intent.getBooleanExtra(Intent.EXTRA_REPLACING, false)) {
+                isReplacing = true;
+            }
 
             // Receive intent from broadcast and simply let the launcher know it needs a refresh.
             if (intent.getAction().equals("android.intent.action.PACKAGE_ADDED")
-                    || intent.getAction().equals("android.intent.action.PACKAGE_CHANGED")
                     || intent.getAction().equals("android.intent.action.PACKAGE_REMOVED")
                     && !packageName.contains(context.getPackageName()) && !isReplacing) {
-                editor.putBoolean("refreshList", true).apply();
+                PreferenceHelper.getEditor().putBoolean("refreshList", true).apply();
             }
         }
     }
