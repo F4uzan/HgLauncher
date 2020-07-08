be36b5e3f6ed79a68ba0aaf139d1dd7e63eac88d
F4uzan <f4@this.is.f4>
2020-07-08 08:48:07 +0100

    More ProgressBar class changes
    * Clean up imports
    * Reduce width for sleeker look
    * Context shouldn't be null anyway

--- app/src/main/java/mono/hg/views/IndeterminateMaterialProgressBar.kt
+++ app/src/main/java/mono/hg/views/IndeterminateMaterialProgressBar.kt
@@ -3,9 +3,7 @@ package mono.hg.views
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ProgressBar
-import androidx.core.content.ContextCompat
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
-import mono.hg.R
 import mono.hg.helpers.PreferenceHelper
 
 /**
@@ -13,15 +11,15 @@ import mono.hg.helpers.PreferenceHelper
  *
  * Uses [CircularProgressDrawable] to draw its progress bar.
  */
-class IndeterminateMaterialProgressBar(context: Context?, attrs: AttributeSet?) : ProgressBar(context, attrs) {
+class IndeterminateMaterialProgressBar(context: Context, attrs: AttributeSet?) : ProgressBar(context, attrs) {
     companion object {
-        private const val WIDTH_DP = 6
+        private const val WIDTH_DP = 4
     }
 
     init {
         val metrics = resources.displayMetrics
         val screenDensity = metrics.density
-        val drawable = CircularProgressDrawable(context!!)
+        val drawable = CircularProgressDrawable(context)
         drawable.setColorSchemeColors(PreferenceHelper.accent)
         drawable.strokeWidth = WIDTH_DP * screenDensity
         indeterminateDrawable = drawable
