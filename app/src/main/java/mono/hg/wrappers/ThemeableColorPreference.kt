d628bdded0019259a621a6c999f2cf10f7ed296c
F4uzan <f4@this.is.f4>
2020-09-14 11:59:38 +0100

    Allow theming and custom strings in ColorPreference
    This is one giant flaming ball of mess and will definitely need to be
    reviewed before it can get its flaming mess to production.
    
    A future commit should address the strings situation. At the moment, we
    aren't doing new strings to save up effort for the new major release
    (and old strings are re-used instead).

---
+++ app/src/main/java/mono/hg/wrappers/ThemeableColorPreference.kt
@@ -0,0 +1,48 @@
+package mono.hg.wrappers
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.app.AlertDialog
+import com.jaredrummler.android.colorpicker.ColorPickerDialog
+import com.jaredrummler.android.colorpicker.ColorPreferenceCompat
+import com.jaredrummler.android.colorpicker.ColorShape
+import mono.hg.R
+import mono.hg.helpers.PreferenceHelper
+import mono.hg.utils.applyAccent
+
+class ThemeableColorPreference(context: Context?, attrs: AttributeSet?) :
+    ColorPreferenceCompat(context, attrs) {
+
+    override fun onClick() {
+        // TODO: Better strings for production release. At the moment we're scraping with what we have.
+        ColorPickerDialog.newBuilder()
+            .setDialogType(ColorPickerDialog.TYPE_PRESETS)
+            .setDialogTitle(R.string.app_theme_accent_dialog)
+            .setColorShape(ColorShape.CIRCLE)
+            .setPresets(presets)
+            .setAllowPresets(true)
+            .setAllowCustom(true)
+            .setShowAlphaSlider(false)
+            .setShowColorShades(true)
+            .setColor(getPersistedInt(PreferenceHelper.accent))
+            .setPresetsButtonText(R.string.icon_pack_default)
+            .setCustomButtonText(R.string.dialog_action_edit)
+            .setSelectedButtonText(R.string.dialog_ok)
+            .create().apply {
+                setColorPickerDialogListener(this@ThemeableColorPreference)
+            }.also {
+                activity.supportFragmentManager.run {
+                    beginTransaction().add(it, fragmentTag).commitAllowingStateLoss()
+
+                    // Invoke executePendingTransactions to immediately show the dialog.
+                    // FIXME: This might be a hack. Will need to investigate further.
+                    executePendingTransactions()
+                }
+
+                (it.dialog as AlertDialog).apply {
+                    applyAccent()
+                }
+            }
+    }
+
+}
